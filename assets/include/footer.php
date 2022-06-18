50969aee55bb282b1074e1b6bdcbd5422cf34ee4
shabranali786 <shabranoffice786@gmail.com>
2022-06-18 20:27:06 +0500

    first commit

---
+++ assets/include/footer.php
@@ -0,0 +1,53 @@
+<!--################################################### 
+            start footer SECTION
+#############################################-->
+<section class="footer bg-black pb-5">
+    <div class="container">
+        <div class="text-center">
+            <img src="assets/images/logo3.svg" alt="" class="mx-auto mb-3" style="width: 170px;
+            ">
+            <br>
+            <ul class="footer-links d-flex align-items-center flex-wrap justify-content-center gap-5">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#howitwork">How it works</a></li>
+                <li><a href="#RemoteAssistant">Remote Assistants</a></li>
+                <li><a href="#digitalmarketing">Digital Marketing</a></li>
+                <li><a href="#pricing">Pricing</a></li>
+            </ul>
+            <hr class="mx-auto my-4 width-100 width-md-60 width-lg-70">
+            <ul class="social-links d-flex align-items-center flex-wrap justify-content-center gap-3">
+                <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</section>
+<div class="copy-right bg-black">
+    <div class="container">
+        <div class="text-center">
+            <p class="m-0 pb-3 text-white font-16px" style="letter-spacing: 0.5px;">© 2021 All rights reserved. <a href="#" class=" primary-text font-weight-600 hover-opacity-06">Taskofi</a></p>
+        </div>
+    </div>
+</div>
+
+
+<!--################################################### 
+            END Free Consultation SECTION
+#############################################-->
+
+<script src="vendor/js/jquery-3.6.0.min.js"></script>
+<script src="vendor/js/bootstrap/popper.min.js"></script>
+<script src="vendor/js/bootstrap/bootstrap.min.js"></script>
+<script src="vendor/js/fancybox/fancybox.umd.js"></script>
+<script src="vendor/js/title.js/title.js"></script>
+<script src="vendor/css/WOW-master/dist/wow.min.js"></script>
+<script src="vendor/js/splitting/splitting.min.js"></script>
+<script src="vendor/js/scrollreveal/scrollreveal.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>
---
+++ assets/include/header.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="64x64" href="assets/images/fav.png">
+    <meta name="description" content="TASKOFI provides qualified and professional remote assistants to assist your business grow, increase  productivity, and free up your time. whilst keeping your expenses at an all time low.">
+
+    <!-- og -->
+    <meta property="og:description" content="TASKOFI provides qualified and professional remote assistants to assist your business grow, increase  productivity, and free up your time. whilst keeping your expenses at an all time low." />
+    <!-- boostrap -->
+    <link rel="stylesheet" href="vendor/css/bootstrap/bootstrap.min.css">
+    <!-- animations -->
+    <link rel="stylesheet" href="vendor/css/WOW-master/css/libs/animate.css" />
+
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="vendor/css/bootstrap/icons/bootstrap-icons.css" />
+
+    <!-- css file -->
+
+    <link rel="stylesheet" href="vendor/css/splitting/splitting.css" />
+    <link rel="stylesheet" href="https://unpkg.com/splitting/dist/splitting-cells.css" />
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="assets/sass/utility/typography.css">
+    <link rel="stylesheet" href="assets/css/media.css">
+    <title>Taskofi</title>
+</head>
+
+<body>
+    <div class="preloader">
+        <div class="preloader-img">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 142 34" class="mv-svg-title">
+                <linearGradient id="gradation-1">
+                    <stop offset="0%" stop-color="#1bb" />
+                    <stop offset="100%" stop-color="#72BE44" />
+                </linearGradient>
+                <path class="text-path" d="M130.362 13.1196H122.896V16.0022H130.215V22.2109H122.896V29.3065H116.528V6.91089H130.289V13.1196H130.362Z" fill="#72BE44" />
+                <path class="text-path" d="M133.216 6.91089H139.585V29.3065H133.216V6.91089Z" fill="#72BE44" />
+                <path class="text-path" d="M119.382 6.91089H116.381L114.844 13.1196H119.382V6.91089Z" fill="#72BE44" />
+                <path class="text-path" d="M127.214 13.1196H130.142L131.753 6.91089H127.214V13.1196Z" fill="#72BE44" />
+                <path class="text-path" d="M20.7144 6.91089V13.1196H15.5175V29.3065H9.14948V13.1196H3.95258V6.91089H20.7144Z" fill="#16151A" />
+                <path class="text-path" d="M34.1825 29.3065L33.4505 26.4239H27.0824L26.3505 29.3065H19.5433L26.6433 6.91089H34.0361L40.9897 29.3065H34.1825ZM28.4 21.3239H32.0598L30.2299 14.45L28.4 21.3239Z" fill="#16151A" />
+                <path class="text-path" d="M50.0659 29.8239C47.6505 29.8239 45.6742 29.3065 44.0639 28.2718C42.4536 27.237 41.3556 25.9065 40.8433 24.1326L46.2598 20.9544C46.9917 22.8761 48.3092 23.837 50.2855 23.837C51.7494 23.837 52.4082 23.4674 52.4082 22.6544C52.4082 22.4326 52.335 22.2848 52.2618 22.0631C52.1886 21.9152 51.969 21.7674 51.6762 21.6196C51.3835 21.4718 51.0907 21.3978 50.8711 21.3239C50.6515 21.25 50.2855 21.1022 49.7 20.9544C49.1144 20.8065 48.7484 20.6587 48.4556 20.5848C44.0639 19.2544 41.868 16.8152 41.868 13.4152C41.868 11.3457 42.6 9.64567 44.0639 8.31523C45.5278 6.9848 47.4309 6.31958 49.8464 6.31958C51.7494 6.31958 53.433 6.76306 54.8969 7.65001C56.3608 8.53697 57.4587 9.86741 58.1907 11.4935L52.9206 14.7457C52.335 13.1935 51.3103 12.3805 49.8464 12.3805C49.2608 12.3805 48.8948 12.5283 48.602 12.75C48.3092 12.9718 48.1628 13.2674 48.1628 13.5631C48.1628 14.0065 48.3824 14.3761 48.8948 14.6718C49.4072 14.8935 50.3587 15.2631 51.7494 15.6326C52.7742 15.9283 53.6525 16.2239 54.3113 16.5196C54.9701 16.8152 55.702 17.1848 56.434 17.7022C57.1659 18.2196 57.7515 18.8848 58.1175 19.6978C58.4835 20.5109 58.7031 21.4718 58.7031 22.5805C58.7031 24.8718 57.8979 26.6457 56.3608 27.9022C54.8237 29.1587 52.7742 29.8239 50.0659 29.8239Z" fill="#16151A" />
+                <path class="text-path" d="M79.6371 29.3065H72.6103L67.1206 19.6978V29.3065H60.7526V6.91089H67.1206V15.8544L72.3175 6.91089H79.3443L72.7567 17.7761L79.6371 29.3065Z" fill="#16151A" />
+                <path class="text-path" d="M63.6072 6.91089H60.6061L59.069 13.1196H63.6072V6.91089Z" fill="#16151A" />
+                <path class="text-path" d="M6.73401 6.91089H3.73298L2.19586 13.1196H6.73401V6.91089Z" fill="#16151A" />
+                <path class="text-path" d="M17.567 13.1196H20.568L22.1052 6.91089H17.567V13.1196Z" fill="#16151A" />
+                <path class="text-path" d="M83.6629 7.72392C83.5165 7.5761 83.4433 7.35436 83.5165 7.13262C83.5165 6.91088 83.6629 6.76305 83.8093 6.61523C86.0784 4.32392 88.6402 2.84566 91.7145 2.03262C94.6423 1.29349 97.6433 1.29349 100.571 2.03262C103.572 2.84566 106.207 4.39784 108.476 6.61523C108.623 6.76305 108.696 6.91088 108.769 7.13262C108.769 7.35436 108.769 7.5761 108.623 7.72392L106.5 11.1239C106.427 11.2717 106.207 11.4196 105.988 11.4935C105.768 11.5674 105.548 11.5674 105.402 11.4935L101.157 9.79349C101.01 9.71958 100.864 9.57175 100.718 9.42392C100.571 9.2761 100.571 9.05436 100.571 8.83262L100.864 5.8761C99.3268 5.28479 97.7897 4.98914 96.1794 5.06305C94.5691 5.06305 92.9588 5.35871 91.4949 5.8761L91.7876 8.83262C91.7876 9.05436 91.7876 9.20218 91.6413 9.35001C91.5681 9.49784 91.4217 9.64566 91.2021 9.71958L86.9567 11.4196C86.8103 11.4935 86.5907 11.4935 86.3711 11.4196C86.1516 11.3457 86.0052 11.1978 85.8588 11.05L83.6629 7.72392Z" fill="#72BE44" />
+                <path class="text-path" d="M83.6629 23.9848C82.199 23.9848 81.0278 22.8022 81.0278 21.3239V16.3717C81.0278 14.8935 82.199 13.7109 83.6629 13.7109C85.1268 13.7109 86.2979 14.8935 86.2979 16.3717V21.3239C86.2979 22.8022 85.1268 23.9848 83.6629 23.9848Z" fill="#72BE44" />
+                <path class="text-path" d="M108.842 23.9848C107.378 23.9848 106.207 22.8022 106.207 21.3239V16.3717C106.207 14.8935 107.378 13.7109 108.842 13.7109C110.306 13.7109 111.477 14.8935 111.477 16.3717V21.3239C111.551 22.8022 110.306 23.9848 108.842 23.9848Z" fill="#72BE44" />
+                <path class="text-path" d="M96.7649 32.1891H98.2289C98.8144 32.1891 99.3268 31.6717 99.3268 31.0804C99.3268 30.4891 98.8144 29.9717 98.2289 29.9717H96.7649C96.1794 29.9717 95.667 30.4891 95.667 31.0804C95.667 31.6717 96.1794 32.1891 96.7649 32.1891Z" fill="#72BE44" />
+                <path class="text-path" d="M86.0051 26.8674C86.0051 26.8674 90.6897 32.2631 96.4721 31.7457L96.5453 30.2674C96.5453 30.2674 92.0804 31.0804 86.8835 26.7196L86.0051 26.8674Z" fill="#72BE44" />
+            </svg>
+        </div>
+    </div>
+    <!--################################################### 
+                    START NAVBAR
+#############################################-->
+    <div class="cover-navbar">
+        <nav class="navbar navbar-expand-lg bg-white py-3" id="taskofi-navbar">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    <img src="assets/images/logo.svg" alt="image">
+                </a>
+                <button class="navbar-toggler d-lg-none my-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                    <!-- <i class="bi bi-justify-right"></i> -->
+                    <i class="material-symbols-sharp">
+                        format_align_right
+                    </i>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                        <li class="nav-item ">
+                            <a class="nav-link active" href="#home">Home </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#howitwork">How it works</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#RemoteAssistant">Remote Assistants</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#digitalmarketing">Digital Marketing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#pricing">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contact us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link primary-btn border-radius-50px" href="#">Get a Free Consultation</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+
+    <!--################################################### 
+                END NAVBAR
+#############################################-->
